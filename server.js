bc0d30a31d64ad66ed52e32d468d48ce2b742193
brunogouveia <brunovcg@gmail.com>
2021-07-23 07:49:22 -0300

    added info to database

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const auth = require("json-server-auth");
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 const port = process.env.PORT || 3001;
+const cors = require('cors');
 
 app.db = router.db;
 
 const rules = auth.rewriter({
   "/users*": "/600/users$1",
-  "/products*": "/640/products$1"
+  "/info*": "/640/info$1"
 });
 
+app.use(cors());
 app.use(rules);
 app.use(auth);
 app.use(router);
